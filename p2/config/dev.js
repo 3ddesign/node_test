e297b05d51c6c3fd1d0afeafada6e1b691991774
3ddesign <3ddesign@bk.ru>
2018-10-12 22:18:20 +0300

    add mongodb auth2

---
+++ p2/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://admin:123qweasd@ds131373.mlab.com:31373/blogdev',
+  cookieKey: '123123123'
+};
+
