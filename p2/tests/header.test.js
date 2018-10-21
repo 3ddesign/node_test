fcb34c6be26655d701b758f309309907f901c351
3ddesign <3ddesign@bk.ru>
2018-10-21 08:50:22 +0300

    add puppeteer test

--- p2/tests/header.test.js
+++ p2/tests/header.test.js
@@ -1,5 +1,30 @@
-test('Adds two numbers', () => {
-    const sun = 1 + 2;
+const puppeteer = require('puppeteer');
 
-    expect(sun).toEqual(3);
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  jest.setTimeout(30000);
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('the header has the correct text', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
+});
+
+test('clickin login starts oauth flow', async () => {
+  await page.click('.right a'); 
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+
 });
