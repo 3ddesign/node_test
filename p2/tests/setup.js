c00c4f990c5b92b029efac204d09cef7a301fd40
3ddesign <3ddesign@bk.ru>
2018-10-28 07:17:09 +0200

    add test setup file

---
+++ p2/tests/setup.js
@@ -0,0 +1,7 @@
+requiere('../models/User.js');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
