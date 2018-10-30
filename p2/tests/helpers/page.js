f8740ac52a6110c60345c12118c186b5c9e46e90
3ddesign <3ddesign@bk.ru>
2018-10-30 21:52:05 +0200

    implemets proxy

--- p2/tests/helpers/page.js
+++ p2/tests/helpers/page.js
@@ -1,5 +1,12 @@
 const puppeteer = require('puppeteer');
 
 class CustomPage {
-    
+    static async build() {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+
+        const page = await browser.newPage();
+        const customPage = new CustomPage();
+    }
 }
