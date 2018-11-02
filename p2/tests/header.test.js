538a1109488a37c47ceb6e2326f8ed3af3d6627c
3ddesign <3ddesign@bk.ru>
2018-11-02 16:30:09 +0200

    add fixes

--- p2/tests/header.test.js
+++ p2/tests/header.test.js
@@ -1,5 +1,3 @@
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
 const Page = require('./helpers/page');
 
 let page;
@@ -16,7 +14,7 @@ afterEach(async () => {
 
 test('the header has the correct text', async () => {
   jest.setTimeout(30000);
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  const text = await page.getContentsOf('a.brand-logo');
   expect(text).toEqual('Blogster');
 });
 
@@ -27,14 +25,7 @@ test('clickin login starts oauth flow', async () => {
 });
 
 test('When signed in, shows logout button', async () => {
-  // const id = '5bc0f26e4d38f2164ec02ac1';
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-
-  await page.setCookie({ name: 'session', value: session});
-  await page.setCookie({ name: 'session.sig', value: sig});
-  await page.goto('localhost:3000');
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
   expect(text).toEqual('Logout');
 });
