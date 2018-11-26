05e75a9667564a44a67bf044ccba19b8d1ad8207
3ddesign <3ddesign@bk.ru>
2018-11-26 07:36:55 +0200

    add express

--- p3/app.js
+++ p3/app.js
@@ -1,5 +1,20 @@
-const http = require('http');
-const routes = require('./routes');
-const server = http.createServer(routes.handler);
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
